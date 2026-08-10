67b0152e33b6a24252d05fe4b021bab923c80b34
Dhinakarsivakumar <sdhinakar6@gmail.com>
2026-08-10 15:03:27 +0530

    Fix Railway build error by using python -m pip install in nixpacks.toml

--- soloNest/settings.py
+++ soloNest/settings.py
@@ -129,7 +129,7 @@ STORAGES = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.getenv('RAILWAY_VOLUME_MOUNT_PATH', os.path.join(BASE_DIR, 'media'))
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'post_login_redirect'
